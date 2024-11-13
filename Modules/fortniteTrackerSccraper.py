469c3f2644b09872ae7da58518087f715f37eb01
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-11-13 10:22:43 -0800

    Moving of the CLI

---
+++ Modules/fortniteTrackerSccraper.py
@@ -0,0 +1,238 @@
+import pandas
+import os
+import random
+
+from tqdm import tqdm
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class CustomError(Exception):
+    """
+    Custom Error Message Class Allowing for Custom Errors
+    """
+    
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f"{self.message}"
+
+options = webdriver.EdgeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+def scrape_current_rank(jsonData: dict, file_name: str="Output", output_folder:str = None)  -> None:
+  def removeUneeded(rankList: list) -> list:
+    temp = [] 
+    for word in rankList:
+      if "%" not in word and "Ranked" not in word:
+        temp.append(word)
+    return temp
+  
+  df = pandas.DataFrame(columns=["Username", "Ranked BR", "Ranked ZB", "Ranked Reload BR", "Ranked Reload ZB"])
+  for school in tqdm(list(jsonData.keys()),desc="Scraping Ranks             ", bar_format="{l_bar}{bar:30}{r_bar}" ,total=len(list(jsonData.keys()))):
+      for user in jsonData[school]:
+        if user["game_network_username"] is None:
+            continue
+          
+        driver = webdriver.Edge(options)
+        driver.get(f"https://fortnitetracker.com/profile/search?q={user["game_network_username"].replace(" ", "%20")}")    
+        
+          
+        try:
+          error_card = WebDriverWait(driver, random.randint(5,10)).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".trn-card--error")))
+          df.loc[len(df.index)] = [user["game_network_username"], None,None,None,None]
+          driver.close()
+          continue
+        except:
+          pass
+          
+        try:
+          rankText = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".profile-current-ranks__content")))
+          rankText = removeUneeded(rankText.text.split("\n"))
+          rankText.insert(0, user["game_network_username"])
+          df.loc[len(df.index)] = rankText
+          driver.close()
+        except:
+          driver.close()
+          df.loc[len(df.index)] = [user["game_network_username"], None,None,None,None]
+          continue
+  
+  if output_folder is not None:
+    if not os.path.exists(f"./{output_folder}"):       
+      df.to_csv(f"./{output_folder}/{file_name}.csv",encoding="utf-8", index=False, header=True)
+    else:
+      raise CustomError("Output Folder Doesnt Exist")
+  else:
+      df.to_csv(f"{file_name}.csv",encoding="utf-8", index=False, header=True)
+
+def scrape_peak_rank(jsonData: dict, file_name: str="Output", output_folder:str = None) -> None:
+
+  def removeUneeded(rankList: list) -> list:
+    tempList = []
+    for word in rankList:
+      if "%"not in word and "Ranked" not in word and not word.startswith("CH"):
+        tempList.append(word)
+    return tempList
+
+  df = pandas.DataFrame(columns=["Username", "Ranked BR", "Ranked ZB"])
+  for school in tqdm(list(jsonData.keys()), desc="Scraping Ranks", bar_format="{l_bar}{bar:30}{r_bar}", total=len(list(jsonData.keys()))):
+    for user in jsonData[school]:
+      
+      if user["game_network_username"] is None:
+        continue
+      
+      driver = webdriver.Edge(options)
+      driver.get(f"https://fortnitetracker.com/profile/search?q={user["game_network_username"].replace(" ", "%20")}")
+
+      try:
+        error_card = WebDriverWait(driver, random.randint(5, 10)).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".trn-card--error")))
+        df.loc[len(df.index)] = [user["game_network_username"], "Username Not Found", "Username Not Found"]
+        driver.close()
+        continue
+      except:
+        pass
+
+      try:
+        rankText = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".profile-peak-ranks__grid")))
+        rankText = removeUneeded(rankText.text.split("\n"))
+        rankText.insert(0, user["game_network_username"])
+        df.loc[len(df.index)] = rankText
+        driver.close()      
+      except:
+        driver.close()
+        df.loc[len(df.index)] = [user["game_network_username"], "No Peak Rank", "No Peak Rank"]
+        continue
+  
+  if output_folder is not None:
+    if not os.path.exists(f"./{output_folder}"):      
+      df.to_csv(f"./{output_folder}/{file_name}.csv",encoding="utf-8", index=False, header=True)
+    else:
+      raise CustomError("Output Folder Doesnt Exist")
+  else:
+      df.to_csv(f"{file_name}.csv",encoding="utf-8", index=False, header=True)
+
+def scrape_current_team_average(data:dict, file_name: str ="Ouput", output_folder: str = None, mode: str="BR") -> None:
+  parameters = ["BR", "ZB", "RBR", "RZB"]
+  
+  if mode not in parameters:
+    raise CustomError(f"{mode} not a permitted parameter: {["BR", "ZB", "RBR", "RZB"]}")
+  default_rank = "Gold 2"
+  ranks = ["Unrated", "Bronze 1", "Bronze 2", "Bronze 3", "Silver 1", "Silver 2", "Silver 3", "Gold 1", "Gold 2", "Gold 3", "Platinum 1", "Platinum 2", "Platinum 3", "Diamond 1", "Diamond 2", "Diamond 3", "Elite", "Champion", "Unreal"]
+  
+  def rank_to_int(rankValue:str) -> int:
+    if rankValue not in ranks:
+      raise CustomError(f"Rank not in ranks list: {rankValue}")
+   
+    return (len(ranks) - ranks.index(rankValue))
+  
+  def removeUneeded(rankList: list) -> list:
+    temp = [] 
+    for word in rankList:
+      if "%" not in word and "Ranked" not in word:
+        temp.append(word)
+    return temp
+  
+  df = pandas.DataFrame(columns=["School Name", "Average Rank"])
+  for school in list(data.keys()):
+    
+    team_average = 0
+    temp = []
+    for player in tqdm(data[school], desc=f"Scraping {school} Average", bar_format="{l_bar}{bar:30}{r_bar}", total=len(data[school])):
+      
+      if player["game_network_username"] is None:
+        continue
+
+      driver = webdriver.Edge(options)
+      driver.get(f"https://fortnitetracker.com/profile/search?q={player["game_network_username"].replace(" ", "%20")}")
+
+      try:
+        error_card = WebDriverWait(driver, random.randint(5, 10)).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".trn-card--error")))
+        driver.close()
+        team_average += rank_to_int(default_rank)
+        continue
+      except:
+        pass
+
+      try:
+        rank_text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".profile-current-ranks__content")))
+        rank_text = removeUneeded(rank_text.text.split("\n"))
+        team_average += rank_to_int(rank_text[parameters.index(mode)])
+        driver.close()
+      except:
+        driver.close()
+        continue    
+    
+    df.loc[len(df.index)] = [school, ranks[int(team_average / len(school))]]
+  
+  if output_folder is not None:
+    if not os.path.exists(f"./{output_folder}"): 
+      df.to_csv(f"./{output_folder}/{file_name}.csv",encoding="utf-8", index=False, header=True)
+    else:
+      raise CustomError("Output Folder Doesnt Exist")
+  else:
+      df.to_csv(f"{file_name}.csv",encoding="utf-8", index=False, header=True)
+
+def scrape_peak_team_average(data:dict, file_name: str ="Ouput", output_folder: str = None, mode: str="BR") -> None:
+  parameters = ["BR", "ZB"]
+  
+  if mode not in parameters:
+    raise CustomError(f"{mode} not a permitted parameter: {parameters}")
+  default_rank = "Gold 2"
+  ranks = ["Unrated", "Bronze 1", "Bronze 2", "Bronze 3", "Silver 1", "Silver 2", "Silver 3", "Gold 1", "Gold 2", "Gold 3", "Platinum 1", "Platinum 2", "Platinum 3", "Diamond 1", "Diamond 2", "Diamond 3", "Elite", "Champion", "Unreal"]
+  
+  def rank_to_int(rankValue:str) -> int:
+    if rankValue not in ranks:
+      raise CustomError(f"Rank not in ranks list: {rankValue}")
+   
+    return (len(ranks) - ranks.index(rankValue))
+  
+  def removeUneeded(rankList: list) -> list:
+    temp = [] 
+    for word in rankList:
+      if "%" not in word and "Ranked" not in word:
+        temp.append(word)
+    return temp
+
+  df = pandas.DataFrame(columns=["School Name", "Average Rank"])
+  
+  for school in list(data.keys()):
+    
+    team_average = 0
+    temp = []
+    for player in tqdm(data[school], desc=f"Scraping {school} Average", bar_format="{l_bar}{bar:30}{r_bar}", total=len(data[school])):
+            
+      if player["game_network_username"] is None:
+        continue
+
+      driver = webdriver.Edge(options)
+      driver.get(f"https://fortnitetracker.com/profile/search?q={player["game_network_username"].replace(" ", "%20")}")
+
+      try:
+        error_card = WebDriverWait(driver, random.randint(5, 10)).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".trn-card--error")))
+        driver.close()
+        team_average += rank_to_int(default_rank)
+        continue
+      except:
+        pass
+
+      try:
+        rank_text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".profile-peak-ranks__grid")))
+        rank_text = removeUneeded(rank_text.text.split("\n"))
+        team_average += rank_to_int(rank_text[parameters.index(mode)])
+        driver.close()
+      except:
+        driver.close()
+        continue    
+    
+    df.loc[len(df.index)] = [school, ranks[int(team_average / len(school))]]
+  
+  if output_folder is not None:
+    if not os.path.exists(f"./{output_folder}"):
+      df.to_csv(f"./{output_folder}/{file_name}.csv",encoding="utf-8", index=False, header=True)
+    else:
+      raise CustomError("Output Folder Doesnt Exist")
+  else:
+      df.to_csv(f"{file_name}.csv",encoding="utf-8", index=False, header=True)
---
+++ Modules/valorantTrackerScraper.py
@@ -0,0 +1,148 @@
+import pandas
+import os
+import random
+
+from tqdm import tqdm
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class CustomError(Exception):
+    """
+    Custom Error Message Class Allowing for Custom Errors
+    """
+    
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f"{self.message}"
+
+options = webdriver.EdgeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+def scrape_current_rank(json_data:dict, file_name: str="Output", output_folder: str = None) -> None:
+    df = pandas.DataFrame(columns=["Username", "Team", "Current Rank"])
+    for team in tqdm(list(json_data.keys()), desc="Scraping Current Ranks", bar_format="{l_bar}{bar:30}{r_bar}" , total=len(list(json_data.keys()))):
+        for player in json_data[team]:
+            
+            driver = webdriver.Edge(options)
+            driver.get(f"https://tracker.gg/valorant/profile/riot/{player['game_network_username'].replace(" ", "%20").replace("#", "%23")}/overview")
+            
+            try:
+                error_card = WebDriverWait(driver, random.randint(5, 10)).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".content--error")))
+                driver.close()
+                df.loc[len(df.index)] = [player['game_network_username'], "User Not Found or Data Private"]
+                continue
+            except:
+                pass
+            
+            try:
+                rank_text = WebDriverWait(driver, ).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".rating-entry__rank")))
+                df.loc[len(df.index)] = [player['game_network_username'], team ,rank_text.text.split("\n")[1]]
+                driver.close()
+            except:
+                df.loc[len(df.index)] = [player["game_network_username"],team, "No Rank Found"]
+                pass
+                
+    if output_folder is not None:
+        if not os.path.exists(f"./{output_folder}"):       
+            df.to_csv(f"./{output_folder}/{file_name}.csv",encoding="utf-8", index=False, header=True)
+        else:
+            raise CustomError("Output Folder Doesnt Exist")
+    else:
+        df.to_csv(f"{fileName}.csv",encoding="utf-8", index=False, header=True)
+    
+def scrape_peak_rank(json_data: dict, file_name:str = "Output", output_folder: str = None) -> None:
+    df = pandas.DataFrame(columns=["Username", "Team", "Peak Rank", "Peak Act/Episode"])
+    for team in tqdm(list(json_data.keys()), desc="Scraping Peak Ranks", bar_format="{l_bar}{bar:30}{r_bar}" , total=len(list(json_data.keys()))):
+        for player in json_data[team]:
+            driver = webdriver.Edge(options)
+            driver.get(f"https://tracker.gg/valorant/profile/riot/{player['game_network_username'].replace(" ", "%20").replace("#", "%23")}/overview")
+            
+            try:
+                error_card = WebDriverWait(driver, random.randint(5,10)).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".content--error")))
+                driver.close()
+                
+                df.loc[len(df.index)] = [player['game_network_username'], team, "Private or Not Real", "Unkown"]
+                continue
+            except:
+                pass
+            
+            try:
+                rank_text = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".rating-summary__content--secondary")))
+                rank_text = rank_text.text.split("\n")
+                rank_text.pop(rank_text.index("Peak Rating"))
+                if len(rank_text) > 2:
+                    for word in rank_text:
+                        if word.endswith("RR"):
+                            rank_text.pop(rank_text.index(word))         
+                rank_text.insert(0, team)
+                rank_text.insert(0, player["game_network_username"])
+                df.loc[len(df.index)] = rank_text
+            except:
+                df.loc[len(df.index)] = [player["game_network_username"], team, "No Peak Found", "Unknown"]
+                pass
+    
+    if output_folder is not None:
+        if not os.path.exists(f"./{output_folder}"):       
+            df.to_csv(f"./{output_folder}/{file_name}.csv",encoding="utf-8", index=False, header=True)
+        else:
+            raise CustomError("Output Folder Doesnt Exist")
+    else:
+        df.to_csv(f"{file_name}.csv",encoding="utf-8", index=False, header=True)
+
+def scrape_current_team_average(json_data: dict, file_name:str = "Output", output_folder: str = None) -> None:
+    df = pandas.DataFrame(columns=["School Name",  "Team Average"])
+    rank_list = [
+    "Unrated", "Iron 1", "Iron 2", "Iron 3",
+    "Bronze 1", "Bronze 2", "Bronze 3",
+    "Silver 1", "Silver 2", "Silver 3",
+    "Gold 1", "Gold 2", "Gold 3",
+    "Platinum 1", "Platinum 2", "Platinum 3",
+    "Diamond 1", "Diamond 2", "Diamond 3",
+    "Ascendant 1", "Ascendant 2", "Ascendant 3",
+    "Immortal 1", "Immortal 2", "Immortal 3",
+    "Radiant"
+]
+    def rank_to_int(rank:str) -> int:
+        if rank not in rank_list:
+            raise CustomError(f"Rank not in Rank List: {rank}")
+        return len(rank_list) - rank_list.index(rank)
+    
+    
+    for team in tqdm(list(json_data.keys()), desc="Scraping Team Current Average", bar_format="{l_bar}{bar:30}{r_bar}", total=len(list(json_data.keys()))):    
+        team_average = 0
+        for  player in json_data[team]:
+            
+            driver = webdriver.Edge(options)
+            driver.get(f"https://tracker.gg/valorant/profile/riot/{player['game_network_username'].replace(" ", "%20").replace("#", "%23")}/overview")
+            
+            try:
+                error_card = WebDriverWait(driver, random.randint(5, 10)).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".content--error")))
+                driver.close()
+                team_average += rank_to_int("Gold 1")
+                continue
+            except:
+                pass
+            
+            try:
+                rank_text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".rating-entry__rank")))
+                team_average += rank_to_int(rank_text.text.split("\n")[1])
+                driver.close()
+            except:
+                team_average += rank_to_int("Gold 1")
+                driver.close()
+                continue 
+                
+        df.loc[len(df.index)] = [team, rank_list[int(team_average/len(json_data[team]))]]
+        
+    if output_folder is not None:
+        if not os.path.exists(f"./{output_folder}"):       
+            df.to_csv(f"./{output_folder}/{file_name}.csv",encoding="utf-8", index=False, header=True)
+        else:
+            raise CustomError("Output Folder Doesnt Exist")
+    else:
+        df.to_csv(f"{file_name}.csv",encoding="utf-8", index=False, header=True)
