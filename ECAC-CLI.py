735e24b9555acf7a61334d6094246c67305cd0d7
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-11-13 21:10:24 -0800

    Added Fortnite Scraper

--- ECAC-CLI.py
+++ ECAC-CLI.py
@@ -1,4 +1,9 @@
-from Modules import ECAC
+#Custom Modules 
+import Modules.ECAC as ECAC
+import Modules.fortniteTrackerSccraper as FTS
+import Modules.valorantTrackerScraper as VTS
+
+#Installed Modules
 import typer
 import os
 import json
@@ -22,29 +27,26 @@ def read_file(api_key_path: str) -> str:
         print(f"Error:\n{e}")
 
 def get_key(api_key_path:str) -> str:
+    if os.path.isfile(api_key_path) is False:
+        raise CustomError(f"File Doesnt Exist: {api_key_path}")
+    if api_key_path.endswith(".txt") is False:
+        raise CustomError("API key file must be a .txt file")
+    
     key = read_file(api_key_path)
     
     if not key.startswith("Bearer"):
         raise CustomError("Incorrect API Key, Must start with Bearer")
     return key
-    
+
+def check_output_folder(output_folder: str) -> None:
+    if os.path.isdir(output_folder) is False:
+        os.mkdir(f"./{output_folder}")
+
 @app.command()
-def scrape_users(api_key_path: str, comp_id: int, network: str, bracket_id: int = None, ouput_folder: str = "Ouput"):
+def scrape_users(api_key_path: str, comp_id: int, network: str, bracket_id: int = None, output_folder: str = "Output"):
+    
     #Check Vars
-    if not os.path.isfile(api_key_path):
-        raise CustomError(f"File Doesnt Exist: {api_key_path}")
-    
-    if not api_key_path.endswith(".txt"):
-        raise CustomError("Api key file must be a .txt file")
-    
-    if ouput_folder == "Output":
-        if not os.path.isdir(ouput_folder):
-                os.mkdir("./Ouput")
-        else:
-            pass
-    else:
-        if not os.path.isdir(ouput_folder):
-            os.mkdir(f"./{ouput_folder}")
+    check_output_folder(output_folder)
     
     #Set ECAC Vars
     ECAC.ECAC_API_header.set(get_key(api_key_path))
@@ -55,7 +57,7 @@ def scrape_users(api_key_path: str, comp_id: int, network: str, bracket_id: int 
     ECAC.comp_details.scrape_details()
     
     #Actual Processor
-    file = open(f"./{ouput_folder}/{ECAC.comp_details.read()['name'].replace(" ", "-")}-users.json", "w", encoding="utf-8")
+    file = open(f"./{output_folder}/{ECAC.comp_details.read()['name'].replace(" ", "-")}-users.json", "w", encoding="utf-8")
     
     if bracket_id is None:
         file.write(json.dumps(ECAC.process_contact_info(ECAC.scrape_team_ids()), ensure_ascii=False))
@@ -65,8 +67,32 @@ def scrape_users(api_key_path: str, comp_id: int, network: str, bracket_id: int 
         file.close()
 
 @app.command()
-def fortnite_stats():
-    pass
+def fortnite_stats(api_key_path: str, comp_id: int, mode: str = "BR", team_stats: bool = False,peak_rank:bool = False, current_rank: bool = False, output_folder: str = "Output", bracket_id: int = None) -> None:
+    #Check Vars    
+    check_output_folder(output_folder)
+    
+    #set ECAC Vars
+    ECAC.ECAC_API_header.set(get_key(api_key_path))
+    ECAC.comp_details.set_id(comp_id)
+    ECAC.network ="EPIC"
+    ECAC.comp_details.scrape_details()
+    
+    if not peak_rank and not current_rank:
+        raise CustomError("Missing Parameters for Ranks wanted: Peak and/or Current Rank")
+    
+    if team_stats:
+        if peak_rank:
+            FTS.scrape_peak_team_average(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), file_name=ECAC.comp_details.read()["name"].replace(" ", "-")+"-peak-team-avrerage", output_folder=output_folder, mode= "Br" if mode != "ZB" else "ZB")
+        if current_rank:
+            FTS.scrape_current_team_average(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), file_name=ECAC.comp_details.read()["name"].replace(" ", "-")+"-current-team-average", output_folder=output_folder, mode= "BR" if mode != "ZB" else "ZB")
+    else:
+        if peak_rank:
+            FTS.scrape_peak_rank(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), file_name=ECAC.comp_details.read()["name"].replace(" ", "-")+"-current-rank", output_folder=output_folder)
+        if current_rank:
+            FTS.scrape_current_rank(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), file_name=ECAC.comp_details.read()["name"].replace(" ", "-")+"-current-rank", output_folder=output_folder)
+            
+    
+    
 
 if __name__ == "__main__":
     app()
