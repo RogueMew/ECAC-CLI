f132dc82b57953fcf6eccee07b3d007aedbdc806
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-11-13 22:12:13 -0800

    Added Usage for Valorant Scraper for ECAC Module

--- ECAC-CLI.py
+++ ECAC-CLI.py
@@ -67,7 +67,7 @@ def scrape_users(api_key_path: str, comp_id: int, network: str, bracket_id: int 
         file.close()
 
 @app.command()
-def fortnite_stats(api_key_path: str, comp_id: int, mode: str = "BR", team_stats: bool = False,peak_rank:bool = False, current_rank: bool = False, output_folder: str = "Output", bracket_id: int = None) -> None:
+def fortnite_stats(api_key_path: str, comp_id: int, mode: str = "BR", team_stats: bool = False, peak_rank:bool = False, current_rank: bool = False, output_folder: str = "Output", bracket_id: int = None) -> None:
     #Check Vars    
     check_output_folder(output_folder)
     
@@ -78,7 +78,7 @@ def fortnite_stats(api_key_path: str, comp_id: int, mode: str = "BR", team_stats
     ECAC.comp_details.scrape_details()
     
     if not peak_rank and not current_rank:
-        raise CustomError("Missing Parameters for Ranks wanted: Peak and/or Current Rank")
+        raise CustomError("Missing Parameters for Ranks Wanted: Peak and/or Current Rank")
     
     if team_stats:
         if peak_rank:
@@ -90,7 +90,33 @@ def fortnite_stats(api_key_path: str, comp_id: int, mode: str = "BR", team_stats
             FTS.scrape_peak_rank(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), file_name=ECAC.comp_details.read()["name"].replace(" ", "-")+"-current-rank", output_folder=output_folder)
         if current_rank:
             FTS.scrape_current_rank(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), file_name=ECAC.comp_details.read()["name"].replace(" ", "-")+"-current-rank", output_folder=output_folder)
-            
+
+@app.command()
+def valorant_stats(api_key_path:str, comp_id: int, team_stats: bool = False, peak_rank: bool = True, current_rank: bool = False, output_folder: str = "Output", bracket_id: int = None) -> None:
+    
+    #Check Vars
+    check_output_folder(output_folder)
+    
+    #Set ECAC Vars
+    ECAC.ECAC_API_header.set(get_key(api_key_path))
+    ECAC.comp_details.set_id(comp_id)
+    ECAC.network = "RIOT"
+    ECAC.comp_details.scrape_details()
+    
+    if not peak_rank and not current_rank:
+        raise CustomError("Missing Parameters for Ranks Wanted: Peak and/or Current Rank")
+    
+    if team_stats:
+        if current_rank:
+            VTS.scrape_current_team_average(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), ECAC.comp_details.read()["name"].replace(" ", "-")+"-current-team-average", output_folder)
+        else:
+            raise CustomError("Peak Rank is not Currently Supported")
+    else:
+        if current_rank:
+            VTS.scrape_current_rank(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), ECAC.comp_details.read()["name"].replace(" ", "-")+"-current-rank", output_folder)
+        
+        if peak_rank:
+            VTS.scrape_peak_rank(ECAC.process_contact_info(ECAC.scrape_team_ids() if bracket_id is None else ECAC.scrape_team_ids_bracket(bracket_id)), ECAC.comp_details.read()["name"].replace(" ", "-")+"-peak-rank", output_folder)
     
     
 
